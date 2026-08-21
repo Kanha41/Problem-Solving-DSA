e6e723987a00a0896c1efed0334f34423d440117
Kanhaiya Yadav <kanhaiyyaydav987@gmail.com>
2026-08-21 23:38:20 +0530

    LeetCode 283: Move Zeros

---
+++ Array.cpp/Leetcode6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        // int i=arr[0];
+        // int j=i+1;
+
+        for(int i=0; i<nums.size(); i++){
+          if(nums[i]==0){
+            int j=i+1;
+            while(j<nums.size() && nums[j]==0){
+                j++;
+            }
+            if(j<nums.size()){
+                swap(nums[i],nums[j]);
+          }
+        }
+    }
+  }
+};
